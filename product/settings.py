4a2e9765129d0bd962d79417c80c7d0061b9d94c
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2020-08-01 17:31:12 +0900

    :gear:radiko のエリア ID も設定に切り出した

--- product/settings.py
+++ product/settings.py
@@ -6,3 +6,4 @@ load_dotenv(verbose=True)
 
 AUDIO_OUTPUT_DIR_PATH = os.getenv("AUDIO_OUTPUT_DIR_PATH", "./work/output")
 LOG_FILE_PATH = os.getenv("LOG_FILE_PATH", "./work/log/app.log")
+RADIKO_AREA_ID = os.getenv("RADIKO_AREA_ID", "JP14")
